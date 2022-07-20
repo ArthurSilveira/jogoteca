480384308e88ccf11a17018befe742f6729d85f4
ArthurSilveira <arthursilveira75@gmail.com>
2022-07-20 12:10:46 -0300

    aula4

--- jogoteca.py
+++ jogoteca.py
@@ -19,4 +19,8 @@ app = Flask(__name__)
 def ola():
     return render_template('lista.html',titulo='Meus Jogos', jogos=lista)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html',titulo='Novo Jogo')
+
 app.run()
