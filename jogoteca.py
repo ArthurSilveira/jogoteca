5ca848834b36901c716319901cb7d31772a36786
Arthur Silveira <108816113+ArthurSilveira@users.noreply.github.com>
2022-08-01 19:27:24 +0000

    adicionei cadastro de jogos

--- jogoteca.py
+++ jogoteca.py
@@ -1,26 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-class jogo:
+
+class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
-jogo1 = jogo('Valorant','Fps','PC')
-jogo2 = jogo('CS:GO','Fps','PC')
-jogo3 = jogo('Minecraft','Aventura','PC/Xbox')
-jogo4 = jogo('LoL','Moba','PC')
+jogo1 = Jogo('Valorant', 'Fps', 'PC')
+jogo2 = Jogo('CS:GO', 'Fps', 'PC')
+jogo3 = Jogo('Minecraft', 'Aventura', 'PC/Xbox')
+jogo4 = Jogo('LoL', 'Moba', 'PC')
 
 lista = [jogo1, jogo2, jogo3, jogo4]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
+
+@app.route('/')
 def ola():
-    return render_template('lista.html',titulo='Meus Jogos', jogos=lista)
+    return render_template('lista.html', titulo='Meus Jogos', jogos=lista)
+
 
 @app.route('/novo')
 def novo():
-    return render_template('novo.html',titulo='Novo Jogo')
+    return render_template('novo.html', titulo='Meus Jogos')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+# Esse código é para quando for rodar no Replit
+app.run(host='0.0.0.0', debug=True)
+
+# Esse código é para quando for rodar em sua máquina
+# app.run(debug=True)
 
 app.run()
